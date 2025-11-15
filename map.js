3590e5e279e0920226203a8a326d497952df81a1
terantaon <trtgong@gmail.com>
2025-11-14 21:27:42 -0800

    Update map.js

--- map.js
+++ map.js
@@ -20,33 +20,31 @@ map.on('load', async () => {
         type: 'geojson',
         data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
     });
-});
 
-map.addLayer({
-  id: 'bike-lanes',
-  type: 'line',
-  source: 'boston_route',
-  paint: {
-    'line-color': 'green',
-    'line-width': 3,
-    'line-opacity': 0.4,
-  },
-});
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': 'green',
+            'line-width': 3,
+            'line-opacity': 0.4,
+        },
+    });
 
-map.on('load', async () => {
     map.addSource('cambridge_route', {
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
     });
-});
 
-map.addLayer({
-  id: 'bike-lanes',
-  type: 'line',
-  source: 'cambridge_route',
-  paint: {
-    'line-color': 'green',
-    'line-width': 3,
-    'line-opacity': 0.4,
-  },
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': 'green',
+            'line-width': 3,
+            'line-opacity': 0.4,
+        },
+    });
 });
